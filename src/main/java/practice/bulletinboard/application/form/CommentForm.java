3e12a3e4ccc0c80ca15e1edf5c0ba2c3988ca822
nikifuna <kbkbfnkb@gmail.com>
2023-02-16 15:03:24 +0900

    Springのバージョンを変更（最新バージョンだと教材と同じ動作にならなかったため）

--- src/main/java/practice/bulletinboard/application/form/CommentForm.java
+++ src/main/java/practice/bulletinboard/application/form/CommentForm.java
@@ -1,10 +1,11 @@
 package practice.bulletinboard.application.form;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.lang.Nullable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class CommentForm {
